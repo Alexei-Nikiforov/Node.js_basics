5ff4c37e3f6296ef4edb24987ea0d8ee558c8c2c
Alexei <Nikifrov_alexei@mail.ru>
2024-12-07 09:59:45 +0300

    DZ2-prav

--- dz_2/index.js
+++ dz_2/index.js
@@ -1,3 +1,13 @@
-const { generatePassword } = require('./generatePassword.js')
+// Функция для генерации случайного пароля из 12 символов
+function generatePassword() {
+    // Массив символов для включения в пароль
+    const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*";
+    let password = "";
+    // Цикл для выбора случайных символов и добавления их в пароль
+    for (let i = 0; i < 12; i++) {
+      password += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return password;
+};
 
 console.log(`Password: ${generatePassword()}`);
